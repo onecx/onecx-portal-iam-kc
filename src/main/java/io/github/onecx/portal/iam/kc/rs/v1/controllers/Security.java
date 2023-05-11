2b6dced71f88e4b9ba70759e075c48f523526d02
Andrej Petras <andrej@lorislab.org>
2023-05-11 16:55:54 +0200

    chore(ci): fix source code validation

--- src/main/java/io/github/onecx/portal/iam/kc/rs/v1/controllers/Security.java
+++ src/main/java/io/github/onecx/portal/iam/kc/rs/v1/controllers/Security.java
@@ -5,4 +5,7 @@ public final class Security {
     public static final String ROLE_PORTAL_SUPER_ADMIN = "onecx-portal-super-admin";
     public static final String ROLE_PORTAL_USER = "onecx-portal-user";
 
+    private Security() {
+        // ignore
+    }
 }
